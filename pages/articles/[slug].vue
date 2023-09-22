6cb2615c91bd6e7d33c1e483b99a573d4f1cb9d8
Cheick Ahmed SIDIBE <chs.ahmed_pro02@outlook.com>
2023-09-22 14:13:12 +0200

    Fix build issue

--- pages/articles/[slug].vue
+++ pages/articles/[slug].vue
@@ -1,21 +1,39 @@
 <script setup>
+import { ref } from 'vue'
+
 const { path } = useRoute()
+const router = useRouter()
 
-const { data } = await useAsyncData(`content${path}`, () => queryContent(`${path}`).findOne())
+const state = ref({
+    title: '',
+    description: '',
+    date: ''
+})
 
-const { title, description, date } = data.value
+try {
+    const { data } = await useAsyncData(`content${path}`, () => queryContent(`${path}`).findOne())
+
+    const { title, description, date } = data.value
+
+    state.value.title = title
+    state.value.description = description
+    state.value.date = date
+} catch (e) {
+    router.push('/')
+}
+
 
 useHead({
-    title: title,
+    title: state.title,
     meta: [
-        { name: 'description', content: description }
+        { name: 'description', content: state.description }
     ],
 })
 </script>
 
 
 <template>
-    <ArticleLayout :meta="{ title, description, date }">
+    <ArticleLayout :meta="{ title: state.title, description: state.description, date: state.date }">
         <ContentDoc />
     </ArticleLayout>
 </template>
