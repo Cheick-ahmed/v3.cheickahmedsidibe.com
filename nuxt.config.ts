671706d96971e1e279233b137d9422721327c9a0
Cheick Ahmed SIDIBE <chs.ahmed_pro02@outlook.com>
2023-09-22 14:43:53 +0200

    Update Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,4 +11,11 @@ export default defineNuxtConfig({
     },
   },
   modules: ['@nuxt/content'],
+  generate: {
+    async routes() {
+      const { $content } = require('@nuxt/content')
+      const files = await $content({ deep: true }).only(['path']).fetch()
+      return files.map((file) => (file.path === '/index' ? '/' : file.path))
+    },
+  },
 })
